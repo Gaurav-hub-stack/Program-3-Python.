7bf27af3e58d3df505e313e8bf9cd94bfba6b4c5
Gaurav-hub-stack <115688930+Gaurav-hub-stack@users.noreply.github.com>
2023-10-15 12:47:21 +0530

    Add files via upload

---
+++ p1/new1.py
@@ -0,0 +1,65 @@
+ #integer representations 
+#1. decimal    0-9      (base 10) default
+#octal         0-7      (base 8)
+#Binary        0-1      (base 2)
+#hexa          0-9a-f/A-F (base 16)
+
+
+#Bitwise Operator 
+a = 5
+b = 3
+
+# Bitwise AND
+result_and = a & b
+print(6&5)
+# Bitwise OR
+result_or = a | b
+print(5|3)
+# Bitwise XOR
+result_xor = a ^ b
+print(5^4)
+# Bitwise NOT
+result_not = ~a
+print(~5)
+# Bitwise left shift
+result_left_shift = a << 2
+print(5<<2)
+# Bitwise right shift
+result_right_shift = a >> 2
+print(5>>2)
+   #### Type Conversion 
+# Integer to float
+a = 5
+a = float(a)
+
+# Float to integer
+b = 3.14
+b = int(b)
+
+# Integer to string
+c = 10
+c = str(c)
+
+# String to integer
+d = "20"
+d = int(d)
+
+# Float to string
+e = 3.14
+e = str(e)
+
+# String to float
+f = "2.5"
+f = float(f)
+x = 4 
+print(str(x))
+### User Input
+
+name = input("Enter your name: ")
+print(name)
+age = input("Enter your Age: ")
+Print(age)
+#WAP to user input to sum
+a=float(input("enter !st nu:"))
+b=float(inout("enter 2nd num:"))
+print(a+b)
